dd76b5dc39f38ee6962419274c541e4eb9b5409f
an-ger <2455927306@qq.com>
2020-07-05 16:23:11 +0800

    P25

---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+const LikeButton: React.FC = () => {
+  const [like, setLike] = useState(0);
+  const [on, setOn] = useState(true)
+  return (
+    <>
+      <button
+        onClick={() => {
+          setLike(like + 1);
+        }}
+      >
+        {like}👍🏻
+      </button>
+      <button
+        onClick={() => {
+          setOn(!on);
+        }}
+      >
+        {on ? 'ON' : 'OFF'}
+      </button>
+    </>
+  );
+};
+export default LikeButton;
