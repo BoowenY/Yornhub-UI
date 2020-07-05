20c752b2dd37cd327cc6ae48bd28fc0a2381ea83
an-ger <2455927306@qq.com>
2020-07-05 22:04:12 +0800

    27

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
-import LikeButton from './components/LikeButton'
-function App() {
+import MouseTracker from './components/MouseTracker'
+const App: React.FC = () => {
+  const [show, setShow] = useState(true)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={()=>{setShow(!show)}}>Toggle Tracker</button>
         </p>
         <Hello message="Hello World"/>
-        <LikeButton/>
+        {show && <MouseTracker/>}
         <a
           className="App-link"
           href="https://reactjs.org"
