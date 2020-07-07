500983d7a223af80d6ec8e5abdc07021289dfdb7
an-ger <2455927306@qq.com>
2020-07-07 22:38:50 +0800

    menu style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Button, {ButtonType, ButtonSize} from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Menu defaultIndex={0} onSelect={(index)=>{alert(index)}}>
+        <MenuItem index={0}>
+        cool link
+        </MenuItem>
+        <MenuItem index={1} disabled>
+        cool link2
+        </MenuItem>
+        <MenuItem index={2}>
+        cool link3
+        </MenuItem>
+      </Menu>
       <header className="App-header">
         <Button onClick={(e) => {e.preventDefault();alert(123)}}>Default Button</Button>
         <Button btnType={ButtonType.Secondary}>Secondary Button</Button>
