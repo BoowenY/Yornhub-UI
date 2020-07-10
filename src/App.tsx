ffd2f0ea718403f8e23aeb33ebcc04930a0cfc86
an-ger <2455927306@qq.com>
2020-07-10 18:27:08 +0800

    6-8

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,41 @@ import React from 'react';
 import Button, {ButtonType, ButtonSize} from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Menu defaultIndex={0} onSelect={(index)=>{alert(index)}}>
+      <Menu defaultIndex={0}>
+        <MenuItem >
+        cool link
+        </MenuItem>
+        <MenuItem  disabled>
+        cool link2
+        </MenuItem>
+        <MenuItem >
+        cool link3
+        </MenuItem>
+      </Menu>
+      <Menu defaultIndex={0} mode="vertical" >
         <MenuItem index={0}>
         cool link
         </MenuItem>
         <MenuItem index={1} disabled>
         cool link2
         </MenuItem>
+        <SubMenu title="fuck">
+          <MenuItem>
+          fuck 1
+          </MenuItem>
+          <MenuItem>
+          fuck 2
+          </MenuItem>
+        </SubMenu>
         <MenuItem index={2}>
         cool link3
         </MenuItem>
       </Menu>
+      
       <header className="App-header">
         <Button onClick={(e) => {e.preventDefault();alert(123)}}>Default Button</Button>
         <Button btnType={ButtonType.Secondary}>Secondary Button</Button>
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,5 +1,6 @@
 import React, {useState, createContext} from 'react'
 import classNames from 'classnames'
+import {MenuItemProps} from './menuItem'
 
 type MenuMode = 'horizontal' | 'vertical'
 type SelectCallback = (selectedIndex: number) => void;
@@ -19,7 +20,8 @@ const Menu: React.FC<MenuProps> = (props) => {
     const {className, mode, style, children, defaultIndex, onSelect} = props
     const [currentActive, setActive] = useState(defaultIndex)
     const classes = classNames('Porn-menu', className, {
-        'menu-vertical': mode === 'vertical'
+        'menu-vertical': mode === 'vertical',
+        'menu-horizontal':mode !== 'vertical'
     })
     const handleClick = (index: number) => {
         setActive(index)
@@ -31,10 +33,23 @@ const Menu: React.FC<MenuProps> = (props) => {
         index: currentActive ? currentActive : 0,
         onSelect: handleClick,
     }
+    const renderChildren = () => {
+        return React.Children.map(children, (child, index) => {
+            const childElement = child as React.FunctionComponentElement<MenuItemProps>
+            const {displayName} = childElement.type
+            if(displayName === 'MenuItem' || displayName === 'SubMenu'){
+                return React.cloneElement(childElement, {
+                    index
+                })
+            }else{
+                console.error('Warning: Menu has a child which is not a MenuItem')
+            }
+        })
+    }
     return (
         <ul className={classes} style={style}>
             <MenuContext.Provider value={passedContext}>
-            {children}
+            {renderChildren()}
             </MenuContext.Provider>
         </ul>
     )
